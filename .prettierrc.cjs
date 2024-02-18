b9635146723a0fd1f260d53f7bf72a9b60e56235
Pihkaal <pihkaal@proton.me>
2024-02-19 00:36:29 +0100

    feat: expo project setup

---
+++ .prettierrc.cjs
@@ -0,0 +1,17 @@
+// @ts-check
+
+/** @type {import("prettier").Options} */
+const config = {
+  arrowParens: "avoid",
+  bracketSpacing: true,
+  quoteProps: "consistent",
+  jsxSingleQuote: false,
+  printWidth: 80,
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  trailingComma: "all",
+  useTabs: false,
+};
+
+module.exports = config;
